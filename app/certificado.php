ea56d2219af6697f48709a5babeb9f0b66f3d0cb
dicsksonCeron <dicksonceron@gmail.com>
2018-05-25 01:20:36 -0500

    css certificado actualizado

--- app/certificado.php
+++ app/certificado.php
@@ -13,14 +13,15 @@
 
 <body onload="certificado()">
     <div id="certificado">
-        <div id="escudo_unimag"><img src="img/unimag.png" alt="escudo unimag">
+        <div id="escudo_unimag"; style="display: flex; justify-content: center; margin-bottom: 30px;margin-top: 10px;">
+          <img style="width: 100px; height: 100px;" src="https://pse.unimagdalena.edu.co/Content/Imagenes/escudo_unimag_sm.png" alt="escudo unimag">
 
         </div>
         <div class="datos"><span>CERTIFICADO ELECTORAL ELECCIONES 27 DE MAYO DE 2018</span></div>
-        <div class="datos"><p>Cedula número <span id="cedula"></span></p></div>
+        <div class="datos"><p>Cedula: <span id="cedula"></span></p></div>
         <div class="datos"> <p>Nombre: <span id="nombre"></span></p> </div>
         <div class="datos"><p>Apellido: <span id="apellido"></span></p></div>
-        <div class="datos"> <p>Mesa #<span id="mesa"></span></p>  </div>
+        <div class="datos"> <p>Numero de mesa: <span id="mesa"></span></p></div>
         <div id="botones">
           <button class="certif_btn" id="btnImprimir">Imprimir</button>
           <button class="certif_btn" id="btnCorreo">Enviar al correo</button>
