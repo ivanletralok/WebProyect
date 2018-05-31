7b3cb790c06a104a25770738efd4ee1671f814cc
dicsksonCeron <dicksonceron@gmail.com>
2018-05-31 05:31:55 -0500

    actualizado, maneja hora en el servidor

--- app/js/login.js
+++ app/js/login.js
@@ -4,40 +4,97 @@ window.localStorage.removeItem('user');
 $(document).ready(function(){
   $('#formLogin').submit(function(){
     event.preventDefault();
-
-    var username = $('input[name="userName"]').val();
-    var password = $('input[name="password"]').val();
-    $.ajax({
-      method: 'POST',
-      url: '../loginAdmin.php',
-      data: {username:username,password:password,tipo:'admin'},
-      success : function(data,textStatus,XHR){
-        if( data != "null")
-        {
-          let user = $.parseJSON(data);
-          window.localStorage.setItem('user', data);
-         
-          if(user.id_rol == "J")
-          {
-            window.location.href = '../index.php'; 
-           
-          }
-          else if(user.id_rol == "V")
-          {
-            if(user.id_estado_usuario < 2 ){
-              swal("no autorizado");
-            }
-            if(user.id_estado_usuario >= 2){
-               window.location.href = '../tarjeton.php'; 
-            }
     
-          }
-        }else{
-          swal('datos incorrectos');
-        }
-      }
-    })
+    /*let HoraInferior = 7,
+        HoraSuperior = 15,
+        MinutoInferior = 01,
+        MinutoSuperior = 59;
 
+    var Hora = ``
+
+    console.log(Hora);
+    //Entre 7am y 4pm
+    let dt = new Date();
+    let time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    let HoraActual =  parseInt(dt.getHours()),
+        MinutoActual = parseInt(dt.getMinutes());
+
+        if(HoraActual >= HoraInferior && HoraActual <= HoraSuperior){
+          if(MinutoActual >= MinutoInferior && MinutoActual <= MinutoSuperior){
+
+            $.ajax({
+              method: 'POST',
+              url: '../loginAdmin.php',
+              data: {username:username,password:password,tipo:'admin'},
+              success : function(data,textStatus,XHR){
+                if( data != "null")
+                {
+                  let user = $.parseJSON(data);
+                  window.localStorage.setItem('user', data);
+                 
+                  if(user.id_rol == "J")
+                  {
+                    window.location.href = '../index.php'; 
+                   
+                  }
+                  else if(user.id_rol == "V")
+                  {
+                    if(user.id_estado_usuario < 2 ){
+                      swal("no autorizado");
+                    }
+                    if(user.id_estado_usuario >= 2){
+                       window.location.href = '../tarjeton.php'; 
+                    }
+            
+                  }
+                }else{
+                  swal('datos incorrectos');
+                }
+              }
+            })
+            
+
+          }          
+        }else{
+          swal('Aun no estan activadas las votaciones, por favor intente luego');
+        }
+        */
+      var username = $('input[name="userName"]').val();
+      var password = $('input[name="password"]').val();
+
+       $.ajax({
+        method: 'POST',
+        url: '../loginAdmin.php',
+        data: {username:username,password:password,tipo:'admin'},
+        success : function(data,textStatus,XHR){
+          if( data != "null")
+          {
+            //console.log(data); 
+            let user = $.parseJSON(data);
+            window.localStorage.setItem('user', data);
+           
+            if(user.id_rol == "J")
+            {
+              window.location.href = '../index.php'; 
+             
+            }
+            else if(user.id_rol == "V")
+            {
+              if(user.id_estado_usuario < 2 ){
+                swal("no autorizado");
+              }
+              if(user.id_estado_usuario >= 2){
+                 window.location.href = '../tarjeton.php'; 
+              }
+      
+            }
+          }else{
+            swal('datos incorrectos');
+          }
+        }
+      })
+
+    
   })
 
 })
--- app/login/login.php
+++ app/login/login.php
@@ -1,47 +1,68 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+    <?php 
 
-<head>
+        date_default_timezone_set('America/Chicago');
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../Css/stil.css">
+        $info = getdate();
 
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+        $hour = $info['hours'];
+        $min = $info['minutes'];
+        $sec = $info['seconds'];
+        
+        $current_date = date('H:i:s');
+        
+        //$hour = 15;
+        //$min = 0;
+    ?>
 
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-
-
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>Login</title>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../Css/stil.css">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>Login</title>
+    </head>
+    
     <body>
-        <form  id ="formLogin">
-            <a>INGRESAR</a>
-                <input id="user" type="number" name="userName" placeholder="&#128221; CÓDIGO" class="us">
-            </label>
-            <label for="">
-                <input id="pass"  type="password"  name="password" placeholder="&#128273; PASSWORD" class="pass" >
-            </label>
-            <button id="login" class="btn loginButton"  type="submit">Login</button>
-        </form>
 
+    <?php if($hour >= 7 and $hour <= 15): ?>
+        <?php if($min >= 0 and $min <= 59): ?>
+            <form  id ="formLogin">
+                <a>INGRESAR</a>
+                    <input id="user" type="number" name="userName" placeholder="&#128221; CÓDIGO" class="us">
+                </label>
+                <label for="">
+                    <input id="pass"  type="password"  name="password" placeholder="&#128273; PASSWORD" class="pass" >
+                </label>
+                <button id="login" class="btn loginButton"  type="submit">Login</button>
+            </form>
+        <?php else: ?>
 
+            <div class="row" id="alertlog">
+                <div class="col s12"> <h1 class="center-align">Aun no habilitado</h1></div>
+            </div>
+           
+        <?php endif ?>       
+    <?php else: ?>
+            <div class="row" id="alertlog">
+                <div class="container">
+                    <div class="col s12"> <h1 class="center-align">Aún no habilitado</h1></div>
+                </div>
+            </div>
+    <?php endif ?>        
+            
     </body>
-    <script
-  src="http://code.jquery.com/jquery-3.3.1.slim.min.js"
-  integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
-  crossorigin="anonymous"></script>
-                <script src="../js/login.js"></script>
+    
+    <script src="http://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+    <script src="../js/login.js"></script>
 
-   <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-
-
-
 </html>
